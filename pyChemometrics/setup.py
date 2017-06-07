1d60798388bcbcecd9aef91f5e03c9fae93ccda3
Gscorreia89 <gscorreia89@gmail.com>
2017-06-07 09:35:56 +0100

    lda and logistic classifier objects created

--- pyChemometrics/setup.py
+++ pyChemometrics/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='pyChemometrics',
       version='0.1',
       description='...',
-      url='http://github.com/gscorreia89/Chemometrics',
+      url='http://github.com/gscorreia89/pyChemometrics',
       author='Gscorreia',
       author_email='gscorreia89@gmail.com',
       license='tba',
