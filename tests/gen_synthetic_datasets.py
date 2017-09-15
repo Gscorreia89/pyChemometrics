5c79ab8ffe4c7baa4badb928d3b793f69384bc78
Gscorreia89 <gscorreia89@gmail.com>
2017-09-15 13:09:04 +0100

    add R script for cross package comparison

--- tests/gen_synthetic_datasets.py
+++ tests/gen_synthetic_datasets.py
@@ -5,7 +5,6 @@ import sklearn.datasets as datamaker
 
 
 def main():
-
     class_dataset = datamaker.make_classification(n_samples=50, n_features=200, n_informative=25,
                                                   n_redundant=5, n_repeated=0, n_classes=2,
                                                   n_clusters_per_class=2, weights=None,
@@ -22,25 +21,25 @@ def main():
                                                        random_state=35624)
 
     regression_dataset = datamaker.make_regression(n_samples=50, n_features=200,
-                                                    n_informative=25, n_targets=1, bias=0.0, effective_rank=None,
-                                                    tail_strength=0.5, noise=0.0, shuffle=True,
-                                                    coef=False, random_state=35624)
+                                                   n_informative=25, n_targets=1, bias=0.0, effective_rank=None,
+                                                   tail_strength=0.5, noise=0.0, shuffle=True,
+                                                   coef=False, random_state=35624)
 
     regression_block_dataset = datamaker.make_regression(n_samples=50, n_features=200,
-                                                            n_informative=25, n_targets=5,
-                                                            bias=0.0, effective_rank=None,
-                                                            tail_strength=0.5, noise=0.0,
-                                                            shuffle=True, coef=False,
-                                                            random_state=35624)
-
+                                                         n_informative=25, n_targets=5,
+                                                         bias=0.0, effective_rank=None,
+                                                         tail_strength=0.5, noise=0.0,
+                                                         shuffle=True, coef=False,
+                                                         random_state=35624)
+    # Save the 2 class classification dataset
     col_names = ['Class']
     col_names.extend(['Var_' + x for x in map(str, range(class_dataset[0].shape[1]))])
     class_dataset_frame = pds.DataFrame(data=np.c_[class_dataset[1], class_dataset[0]], columns=col_names)
     save_path = os.path.dirname(os.path.abspath("__file__"))
     class_dataset_frame.to_csv(os.path.join(save_path, 'classification_twoclass.csv'), index=False)
 
+    # Save the multiclass classification dataset
     multiclass_dummy_mat = pds.get_dummies(multiclass_dataset[1]).values
-
     col_names = ['Class_Vector']
     col_names.extend(['Class_' + x for x in map(str, range(np.atleast_2d(multiclass_dummy_mat).shape[1]))])
     col_names.extend(['Var_' + x for x in map(str, range(multiclass_dataset[0].shape[1]))])
@@ -49,13 +48,15 @@ def main():
     save_path = os.path.dirname(os.path.abspath("__file__"))
     multiclass_dataset_frame.to_csv(os.path.join(save_path, 'classification_mulitclass.csv'), index=False)
 
-
+    # Save the 1 Y variable regression dataset
     col_names = ['Y']
     col_names.extend(['Var_' + x for x in map(str, range(regression_dataset[0].shape[1]))])
-    regression_dataset_frame = pds.DataFrame(data=np.c_[regression_dataset[1], regression_dataset[0]], columns=col_names)
+    regression_dataset_frame = pds.DataFrame(data=np.c_[regression_dataset[1], regression_dataset[0]],
+                                             columns=col_names)
     save_path = os.path.dirname(os.path.abspath("__file__"))
     regression_dataset_frame.to_csv(os.path.join(save_path, 'regression.csv'), index=False)
 
+    # Save the multi Y regression dataset
     col_names = (['Y_' + x for x in map(str, range(np.atleast_2d(regression_block_dataset[1]).shape[1]))])
     col_names.extend(['Var_' + x for x in map(str, range(regression_block_dataset[0].shape[1]))])
     regression_block_dataframe = pds.DataFrame(data=np.c_[regression_block_dataset[1], multiclass_dataset[0]],
