tests and fixed but in R2X calculation between ChemometricsPLS and ChemometricsPLS-DA in multi-y classification due to score and dummy matrix handling

pyChemometrics/ChemometricsPLS.py
@@ -370,9 +370,9 @@ class ChemometricsPLS(BaseEstimator, RegressorMixin, TransformerMixin):
             if block_to_score == 'y':
                 yscaled = deepcopy(self.y_scaler).fit_transform(y)
                 # Calculate total sum of squares of X and Y for R2X and R2Y calculation
-                tssy = np.sum(yscaled ** 2)
+                tssy = np.sum(np.square(yscaled))
                 ypred = self.y_scaler.transform(ChemometricsPLS.predict(self, x, y=None))
-                rssy = np.sum((yscaled - ypred) ** 2)
+                rssy = np.sum(np.square(yscaled - ypred))
                 R2Y = 1 - (rssy / tssy)
                 return R2Y
             # The prediction here of both X and Y is done using the other block of data only
@@ -381,13 +381,11 @@ class ChemometricsPLS(BaseEstimator, RegressorMixin, TransformerMixin):
             else:
                 xscaled = deepcopy(self.x_scaler).fit_transform(x)
                 # Calculate total sum of squares of X and Y for R2X and R2Y calculation
-                tssx = np.sum(xscaled ** 2)
                 xpred = self.x_scaler.transform(ChemometricsPLS.predict(self, x=None, y=y))
-                tssx = np.sum(xscaled ** 2)
-                rssx = np.sum((xscaled - xpred) ** 2)
+                tssx = np.sum(np.square(xscaled))
+                rssx = np.sum(np.square(xscaled - xpred))
                 R2X = 1 - (rssx / tssx)
                 return R2X
-
         except ValueError as verr:
             raise verr
 
@@ -427,11 +425,8 @@ class ChemometricsPLS(BaseEstimator, RegressorMixin, TransformerMixin):
                     return predicted
                 # Predict X from Y
                 elif y is not None:
-                    # Comply with the sklearn scaler behaviour
-                    if y.ndim == 1:
-                        y = y.reshape(-1, 1)
                     # Going through calculation of U and then X = Ub_uW'
-                    u_scores = self.transform(x=None, y=y)
+                    u_scores = ChemometricsPLS.transform(self, x=None, y=y)
                     predicted = np.dot(np.dot(u_scores, self.b_u), self.weights_w.T)
                     if predicted.ndim == 1:
                         predicted = predicted.reshape(-1, 1)
@@ -791,8 +786,8 @@ class ChemometricsPLS(BaseEstimator, RegressorMixin, TransformerMixin):
             pressx = 0
 
             # Calculate Sum of Squares SS in whole dataset for future calculations
-            ssx = np.sum((cv_pipeline.x_scaler.fit_transform(x)) ** 2)
-            ssy = np.sum((cv_pipeline.y_scaler.fit_transform(y)) ** 2)
+            ssx = np.sum(np.square(cv_pipeline.x_scaler.fit_transform(x)))
+            ssy = np.sum(np.square(cv_pipeline.y_scaler.fit_transform(y)))
 
             # As assessed in the test set..., opposed to PRESS
             R2X_training = np.zeros(ncvrounds)
@@ -846,8 +841,8 @@ class ChemometricsPLS(BaseEstimator, RegressorMixin, TransformerMixin):
                 ypred = cv_pipeline.y_scaler.transform(ypred).squeeze()
                 ytest_scaled = ytest_scaled.squeeze()
 
-                curr_pressx = np.sum((xtest_scaled - xpred) ** 2)
-                curr_pressy = np.sum((ytest_scaled - ypred) ** 2)
+                curr_pressx = np.sum(np.square(xtest_scaled - xpred))
+                curr_pressy = np.sum(np.square(ytest_scaled - ypred))
 
                 R2X_test[cvround] = cv_pipeline.score(xtest, ytest, 'x')
                 R2Y_test[cvround] = cv_pipeline.score(xtest, ytest, 'y')
@@ -1067,9 +1062,9 @@ class ChemometricsPLS(BaseEstimator, RegressorMixin, TransformerMixin):
         """
         pred_scores = self.transform(x)
 
-        x_reconstructed = self.scaler.transform(self.inverse_transform(pred_scores))
-        xscaled = self.scaler.transform(x)
-        residuals = np.sum((xscaled - x_reconstructed)**2, axis=1)
+        x_reconstructed = self.x_scaler.transform(self.inverse_transform(pred_scores))
+        xscaled = self.x_scaler.transform(x)
+        residuals = np.sum(np.square(xscaled - x_reconstructed), axis=1)
         return residuals
 
     def _cummulativefit(self, x, y):
@@ -1096,19 +1091,18 @@ class ChemometricsPLS(BaseEstimator, RegressorMixin, TransformerMixin):
         yscaled = self.y_scaler.transform(y)
 
         # Obtain residual sum of squares for whole data set and per component
-        SSX = np.sum(xscaled ** 2)
-        SSY = np.sum(yscaled ** 2)
+        SSX = np.sum(np.square(xscaled))
+        SSY = np.sum(np.square(yscaled))
         ssx_comp = list()
         ssy_comp = list()
 
         for curr_comp in range(1, self.ncomps + 1):
             model = self._reduce_ncomps(curr_comp)
 
-            ypred = model.y_scaler.transform(ChemometricsPLS.predict(model, x, y=None))
-            xpred = model.x_scaler.transform(ChemometricsPLS.predict(model, x=None, y=y))
-
-            rssy = np.sum((yscaled - ypred) ** 2)
-            rssx = np.sum((xscaled - xpred) ** 2)
+            ypred = self.y_scaler.transform(ChemometricsPLS.predict(model, x, y=None))
+            xpred = self.x_scaler.transform(ChemometricsPLS.predict(model, x=None, y=y))
+            rssy = np.sum(np.square(yscaled - ypred))
+            rssx = np.sum(np.square(xscaled - xpred))
             ssx_comp.append(rssx)
             ssy_comp.append(rssy)
 

pyChemometrics/ChemometricsPLSDA.py
@@ -131,20 +131,18 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
             # multi-class setting
             # Only for PLS: the sklearn LogisticRegression still requires a single vector!
             if self.n_classes > 2:
-                create_dummy_mat = pds.get_dummies(y).values
-                y_pls = create_dummy_mat
-                # Remake the internal logistic regression - option One vs the rest is not used, the user can just
-                # provide a different binary labelled y vector to use it instead.
-                y_pls = self.y_scaler.fit_transform(y_pls)
+                dummy_mat = pds.get_dummies(y).values
+                # If the user wants OneVsRest, etc, provide a different binary labelled y vector to use it instead.
+                y_scaled = self.y_scaler.fit_transform(dummy_mat)
             else:
                 if y.ndim == 1:
                     y = y.reshape(-1, 1)
-                y_pls = self.y_scaler.fit_transform(y).squeeze()
+                y_scaled = self.y_scaler.fit_transform(y)
 
             # The PLS algorithm either gets a single vector in binary classification or a
             # Dummy matrix for the multiple classification case
 
-            self.pls_algorithm.fit(xscaled, y_pls, **fit_params)
+            self.pls_algorithm.fit(xscaled, y_scaled, **fit_params)
 
             # Expose the model parameters - Same as in ChemometricsPLS
             self.loadings_p = self.pls_algorithm.x_loadings_
@@ -175,8 +173,12 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
 
             # Calculate RSSy/RSSx, R2Y/R2X
             # Method inheritance from parent, as in this case we really want the "PLS" only metrics
-            R2Y = ChemometricsPLS.score(self, x=x, y=y_pls, block_to_score='y')
-            R2X = ChemometricsPLS.score(self, x=x, y=y_pls, block_to_score='x')
+            if self.n_classes > 2:
+                R2Y = ChemometricsPLS.score(self, x=x, y=dummy_mat, block_to_score='y')
+                R2X = ChemometricsPLS.score(self, x=x, y=dummy_mat, block_to_score='x')
+            else:
+                R2Y = ChemometricsPLS.score(self, x=x, y=y, block_to_score='y')
+                R2X = ChemometricsPLS.score(self, x=x, y=y, block_to_score='x')
 
             # Grid of False positive ratios to standardize the ROC curves
             # All ROC curves will be interpolated to a constant grid
@@ -221,20 +223,23 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
 
                 # Generate multiple ROC curves - one for each class the multiple class case
                 for predclass in range(self.n_classes):
-                    roc_curve = metrics.roc_curve(y, class_score[:, predclass], pos_label=predclass)
+                    current_roc = metrics.roc_curve(y, class_score[:, predclass], pos_label=predclass)
                     # Interpolate all ROC curves to a finite grid
                     #  Makes it easier to average and compare multiple models - with CV in mind
-                    tpr = roc_curve[1]
-                    fpr = roc_curve[0]
+                    tpr = current_roc[1]
+                    fpr = current_roc[0]
 
                     interpolated_tpr = np.zeros_like(fpr_grid)
                     interpolated_tpr += interp(fpr_grid, fpr, tpr)
-                    roc_curve.append(fpr_grid, interpolated_tpr, roc_curve[2])
+                    roc_curve.append([fpr_grid, interpolated_tpr, current_roc[2]])
                     auc_area.append(metrics.auc(fpr_grid, interpolated_tpr))
 
             # Obtain residual sum of squares for whole data set and per component
             # Same as Chemometrics PLS, this is so we can use VIP's and other metrics as usual
-            cm_fit = self._cummulativefit(x, y_pls)
+            if self.n_classes > 2:
+                cm_fit = self._cummulativefit(x, dummy_mat)
+            else:
+                cm_fit = self._cummulativefit(x, y)
 
             # Assemble the dictionary for storing the model parameters
             self.modelParameters = {'PLS': {'R2Y': R2Y, 'R2X': R2X, 'SSX': cm_fit['SSX'], 'SSY': cm_fit['SSY'],
@@ -281,6 +286,7 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
         :type x: numpy.ndarray, shape [n_samples, n_features] or None
         :param y: Data matrix to fit the PLS model.
         :type y: numpy.ndarray, shape [n_samples, n_features] or None
+        :param boolean dummy_y:
         :return: Latent Variable scores (T) for the X matrix and for the Y vector/matrix (U).
         :rtype: tuple with 2 numpy.ndarray, shape [n_samples, n_comps]
         :raise ValueError: If dimensions of input data are mismatched.
@@ -300,23 +306,20 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
                 elif x is None:
                     # The y variable expected is a single vector with ints as class label - binary
                     # and multiclass classification are allowed but not multilabel so this will work.
-                    # if y.ndim != 1:
-                    #    raise TypeError('Please supply a dummy vector with integer as class membership')
+                    if y.ndim != 1:
+                        raise TypeError('Please supply a dummy vector with integer as class membership')
+
                     # Previously fitted model will already have the number of classes
-                    if self.n_classes <= 2:
+                    # The dummy matrix is created here manually because its possible for the model to be fitted to
+                    # a larger number of classes than what is being passed in transform
+                    # and other methods post-fitting
+                    # If matrix is not dummy, generate the dummy accordingly
+                    if self.n_classes > 2:
+                        y = self.y_scaler.transform(pds.get_dummies(y).values)
+                    else:
                         if y.ndim == 1:
                             y = y.reshape(-1, 1)
                         y = self.y_scaler.transform(y)
-                    else:
-                        # The dummy matrix is created here manually because its possible for the model to be fitted to
-                        # a larger number of classes than what is being passed in transform
-                        # and other methods post-fitting
-                        # If matrix is not dummy, generate the dummy accordingly
-                        if y.ndim == 1:
-                            dummy_matrix = np.zeros((len(y), self.n_classes))
-                            for col in range(self.n_classes):
-                                dummy_matrix[np.where(y == col), col] = 1
-                            y = self.y_scaler.transform(dummy_matrix)
                     # Taking advantage of rotations_y
                     # Otherwise this would be the full calculation U = Y*pinv(CQ')*C
                     U = np.dot(y, self.rotations_cs)
@@ -438,7 +441,7 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
                 # project X onto T - so then we can get
                 pred_scores = self.transform(x=x)
                 # prediction rule - find the closest class mean (centroid) for each sample in the score space
-                closest_class_mean = lambda x: np.argmin(np.linalg.norm((x - self.class_means)/self.modelParameters['PLS']['SSYcomp'], axis=1))
+                closest_class_mean = lambda x: np.argmin(np.linalg.norm((x - self.class_means), axis=1))
                 class_pred = np.apply_along_axis(closest_class_mean, axis=1, arr=pred_scores)
             return class_pred
 
@@ -716,8 +719,8 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
             pressx = 0
 
             # Calculate Sum of Squares SS in whole dataset for future calculations
-            ssx = np.sum((cv_pipeline.x_scaler.fit_transform(x)) ** 2)
-            ssy = np.sum((cv_pipeline._y_scaler.fit_transform(y_pls.reshape(-1, 1))) ** 2)
+            ssx = np.sum(np.square(cv_pipeline.x_scaler.fit_transform(x)))
+            ssy = np.sum(np.square(cv_pipeline._y_scaler.fit_transform(y_pls.reshape(-1, 1))))
 
             # As assessed in the test set..., opposed to PRESS
             R2X_training = np.zeros(ncvrounds)
@@ -769,8 +772,8 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
                 xpred = cv_pipeline.x_scaler.transform(xpred).squeeze()
                 ypred = cv_pipeline._y_scaler.transform(ypred).squeeze()
 
-                curr_pressx = np.sum((xtest_scaled - xpred) ** 2)
-                curr_pressy = np.sum((cv_pipeline._y_scaler.transform(yplstest).squeeze() - ypred) ** 2)
+                curr_pressx = np.sum(np.square(xtest_scaled - xpred))
+                curr_pressy = np.sum(np.square(cv_pipeline._y_scaler.transform(yplstest).squeeze() - ypred))
 
                 R2X_test[cvround] = ChemometricsPLS.score(cv_pipeline, xtest, yplstest, 'x')
                 R2Y_test[cvround] = ChemometricsPLS.score(cv_pipeline, xtest, yplstest, 'y')
@@ -1156,100 +1159,8 @@ class ChemometricsPLSDA(ChemometricsPLS, ClassifierMixin):
 
         x_reconstructed = self.scaler.transform(self.inverse_transform(pred_scores))
         xscaled = self.scaler.transform(x)
-        residuals = np.sum((xscaled - x_reconstructed)**2, axis=1)
+        residuals = np.sum(np.square(xscaled - x_reconstructed), axis=1)
         return residuals
-
-    def _cummulativefit(self, x, y):
-        """
-
-        Measure the cumulative Regression sum of Squares for each individual component.
-        Meant to be used internally by the fit method and VIP calculation.
-
-        :param x: Data matrix to fit the PLS model.
-        :type x: numpy.ndarray, shape [n_samples, n_features]
-        :param y: Data matrix to fit the PLS model.
-        :type y: numpy.ndarray, shape [n_samples, n_features]
-        :return: dictionary object containing the total Regression Sum of Squares and the Sum of Squares
-        per components, for both the X and Y data blocks.
-        :rtype: dict
-        """
-        if y.ndim == 1:
-            y = y.reshape(-1, 1)
-        if x.ndim == 1:
-            x = x.reshape(-1, 1)
-        if self._isfitted is False:
-            raise AttributeError('Fit model first')
-
-        xscaled = self.x_scaler.transform(x)
-        yscaled = self.y_scaler.transform(y)
-
-        # Obtain residual sum of squares for whole data set and per component
-        SSX = np.sum(xscaled ** 2)
-        SSY = np.sum(yscaled ** 2)
-        ssx_comp = list()
-        ssy_comp = list()
-
-        for curr_comp in range(1, self.ncomps + 1):
-            model = self._reduce_ncomps(curr_comp)
-
-            ypred = model.y_scaler.transform(ChemometricsPLS.predict(model, x, y=None))
-            xpred = model.x_scaler.transform(ChemometricsPLS.predict(model, x=None, y=y))
-
-            rssy = np.sum((yscaled - ypred) ** 2)
-            rssx = np.sum((xscaled - xpred) ** 2)
-            ssx_comp.append(rssx)
-            ssy_comp.append(rssy)
-
-        cumulative_fit = {'SSX': SSX, 'SSY': SSY, 'SSXcomp': np.array(ssx_comp), 'SSYcomp': np.array(ssy_comp)}
-
-        return cumulative_fit
-
-    def _reduce_ncomps(self, ncomps):
-        """
-
-        Generate a new model with a smaller set of components.
-        Meant to be used internally by the fit method.
-
-        :param int ncomps: Number of ordered first N components from the original model to be kept.
-        Must be smaller than the ncomps value of the original model.
-        :return ChemometricsPLS object with reduced number of components.
-        :rtype: ChemometricsPLS
-        :raise ValueError: If number of components desired is larger than original number of components
-        :raise AttributeError: If model is not fitted.
-
-        """
-        try:
-            if ncomps > self.ncomps:
-                raise ValueError('Fit a new model with more components instead')
-            if self._isfitted is False:
-                raise AttributeError('Model not Fitted')
-
-            newmodel = deepcopy(self)
-            newmodel._ncomps = ncomps
-
-            newmodel.modelParameters = None
-            newmodel.cvParameters = None
-            newmodel.loadings_p = self.loadings_p[:, 0:ncomps]
-            newmodel.weights_w = self.weights_w[:, 0:ncomps]
-            newmodel.weights_c = self.weights_c[:, 0:ncomps]
-            newmodel.loadings_q = self.loadings_q[:, 0:ncomps]
-            newmodel.rotations_ws = self.rotations_ws[:, 0:ncomps]
-            newmodel.rotations_cs = self.rotations_cs[:, 0:ncomps]
-            newmodel.scores_t = None
-            newmodel.scores_u = None
-            newmodel.b_t = self.b_t[0:ncomps, 0:ncomps]
-            newmodel.b_u = self.b_u[0:ncomps, 0:ncomps]
-
-            # These have to be recalculated from the rotations
-            newmodel.beta_coeffs = np.dot(newmodel.rotations_ws, newmodel.loadings_q.T)
-
-            # NOTE: this "destroys" the internal state of the classifier apart from the PLS components,
-            # but this is only meant to be used inside the fit object and for the VIP calculation.
-            return newmodel
-        except ValueError as verr:
-            raise verr
-        except AttributeError as atter:
-            raise atter
 
     def __deepcopy__(self, memo):
         cls = self.__class__

tests/gen_synthetic_datasets.py
@@ -36,7 +36,7 @@ def main():
     col_names.extend(['Var_' + x for x in map(str, range(class_dataset[0].shape[1]))])
     class_dataset_frame = pds.DataFrame(data=np.c_[class_dataset[1], class_dataset[0]], columns=col_names)
     save_path = os.path.dirname(os.path.abspath("__file__"))
-    class_dataset_frame.to_csv(os.path.join(save_path, 'classification_twoclass.csv'), index=False)
+    class_dataset_frame.to_csv(os.path.join(save_path, './test_data/classification_twoclass.csv'), index=False)
 
     # Save the multiclass classification dataset
     multiclass_dummy_mat = pds.get_dummies(multiclass_dataset[1]).values
@@ -46,7 +46,7 @@ def main():
     multiclass_dataset_frame = pds.DataFrame(data=np.c_[multiclass_dataset[1], multiclass_dummy_mat,
                                                         multiclass_dataset[0]], columns=col_names)
     save_path = os.path.dirname(os.path.abspath("__file__"))
-    multiclass_dataset_frame.to_csv(os.path.join(save_path, 'classification_mulitclass.csv'), index=False)
+    multiclass_dataset_frame.to_csv(os.path.join(save_path, './test_data/classification_multiclass.csv'), index=False)
 
     # Save the 1 Y variable regression dataset
     col_names = ['Y']
@@ -54,7 +54,7 @@ def main():
     regression_dataset_frame = pds.DataFrame(data=np.c_[regression_dataset[1], regression_dataset[0]],
                                              columns=col_names)
     save_path = os.path.dirname(os.path.abspath("__file__"))
-    regression_dataset_frame.to_csv(os.path.join(save_path, 'regression.csv'), index=False)
+    regression_dataset_frame.to_csv(os.path.join(save_path, './test_data/regression.csv'), index=False)
 
     # Save the multi Y regression dataset
     col_names = (['Y_' + x for x in map(str, range(np.atleast_2d(regression_block_dataset[1]).shape[1]))])
@@ -62,7 +62,7 @@ def main():
     regression_block_dataframe = pds.DataFrame(data=np.c_[regression_block_dataset[1], multiclass_dataset[0]],
                                                columns=col_names)
     save_path = os.path.dirname(os.path.abspath("__file__"))
-    regression_block_dataframe.to_csv(os.path.join(save_path, 'regression_multiblock.csv'), index=False)
+    regression_block_dataframe.to_csv(os.path.join(save_path, './test_data/regression_multiblock.csv'), index=False)
 
     return None
 

tests/test_pca.py
@@ -1,48 +1,72 @@
 import unittest
-
+import os
+import pandas as pds
 import numpy as np
 from sklearn.datasets import make_classification
 
-from pyChemometrics import ChemometricsScaler, ChemometricsPLS, ChemometricsPLS_Logistic
+from pyChemometrics import ChemometricsScaler, ChemometricsPCA
 
 """
 Suite of tests to assess correctness of the PCA object.
 Cross - checked with R's pcamethods.
 """
 
-dataset = {'X': np.array([]), 'Y':np.array([])}
+dataset = {'X': np.array([]), 'Y': np.array([])}
 expected_scores = {'t': []}
 expect_R2 = {'X': []}
 expected_loadings = {'p': []}
 expected_prediction = {'x': []}
 
 
-class test_plsobjconsistency(unittest.TestCase):
+class test_pcamodel(unittest.TestCase):
     """
-    Verify agreement of PLS algorithms under different objects and conditions
+
+    Verify outputs of the ChemometricsPCA object
+
     """
 
     def setUp(self):
 
-        # Generate 2 fake classification datasets, one with 2 classes and another with 3
-        self.twoclass_dataset = make_classification(40, n_features=100, n_informative=5, n_redundant=5, n_classes=2)
-        self.three_classdataset = make_classification(40, n_features=100, n_informative=5, n_redundant=5, n_classes=3)
-        y_scaler = ChemometricsScaler(with_mean=False, with_std=False)
-        self.plsreg = ChemometricsPLS(n_comps=3, yscaler=y_scaler)
-        self.plslog = ChemometricsPLS_Logistic(n_comps=3)
+        try:
+            # Generate a fake classification dataset
+            t_dset = pds.read_csv('./test_data/classification_twoclass.csv')
+            self.xmat = t_dset.values
 
-    def test_single_y(self):
-        self.plsreg.fit(self.twoclass_dataset[0], self.twoclass_dataset([1]))
-        self.plslog.fit(self.twoclass_dataset[0], self.twoclass_dataset([1]))
-        self.assertEqual(self.plsreg.rotations_cs, self.plslog.rotations_cs)
+        except (IOError, OSError) as ioerr:
+            os.system('python gen_synthetic_datasets.py')
+            t_dset = pds.read_csv('./test_data/classification_twoclass.csv')
+            self.xmat = t_dset.values
 
-    def test_multi_y(self):
-        self.plsreg.fit(self.threeclass_dataset[0], self.threeclass_dataset([1]))
-        self.plslog.fit(self.threeclass_dataset[0], self.threeclass_dataset([1]))
-        self.assertEqual(self.data.noFeatures, self.noFeat)
+        self.x_scaler = ChemometricsScaler(1)
+        self.pcamodel = ChemometricsPCA(n_comps=3, xscaler=self.x_scaler)
 
-    def test_(self):
-        self.assertEqual()
+    def test_fit(self):
+        self.pcamodel.fit(self.xmat)
+        self.assertAlmostEqual(self.pcamodel.modelParameters['R2X'], expected)
+        self.assertAlmostEqual()
+
+    def test_transform(self):
+        self.assertAlmostEqual(self.pcammodel.transform(self.xmat), self.expected_scores)
+
+    def test_fit_transform(self):
+        self.assertAlmostEqual(self.pcammodel.fit_transform(self.xmat), self.expected_scores)
+
+    def test_cv(self):
+        self.pcamodel.cross_validation(self.xmat)
+        self.assertAlmostEqual(self.pcammodel.cvParameters, self.expectedcvParams)
+
+    def HotellingT2(self):
+        pass
+
+    def test_dmodx(self):
+        pass
+
+    def test_outliers(self):
+        outliers_t2 = self.pcamodel.outlier()
+        outliers_dmodx = self.pcamodel.outlier(self.xmat)
+        self.assertAlmostEqual(self.pcammodel.cvParameters, self.expectedcvParams)
+        return None
+
 
 if __name__ == '__main__':
     unittest.main()

tests/test_plsobjconsistency.py
@@ -1,102 +1,95 @@
 import unittest
 import numpy as np
-from numpy.testing import assert_array_equal
-from sklearn.datasets import make_classification
+from numpy.testing import assert_allclose
+import pandas as pds
+import os
 
-from pyChemometrics import ChemometricsScaler, ChemometricsPLS, ChemometricsPLS_Logistic, \
-    ChemometricsPLSDA, ChemometricsPLS_LDA
+from pyChemometrics import ChemometricsScaler, ChemometricsPLS, ChemometricsPLSDA
 
 """
+
 Suite of tests to ensure that all PLS objects are consistent among each other. 
 For example, the ChemometricsPLS object for regression analysis needs to give the same results (coefficients, 
 loadings, scores R2s, etc) as the PLS component of the PLS Classifier objects, 
 provided that we account for the differences in data input and class vector do dummy conversions.
+
 """
 
 
 class test_plsobjconsistency(unittest.TestCase):
     """
 
-    Verify agreement of PLS algorithms under different objects and conditions
+    Verify agreement of the PLS regression component between ChemometricsPLS and Discriminant analysis versions.
 
     """
 
     def setUp(self):
 
-        # Generate 2 fake classification datasets, one with 2 classes and another with 3
-        self.twoclass_dataset = make_classification(40, n_features=100, n_informative=5, n_redundant=5, n_classes=2)
-        self.three_classdataset = make_classification(40, n_features=100, n_informative=5, n_redundant=5, n_classes=3)
-        # Set up the same y_scalers
-        y_scaler = ChemometricsScaler(with_mean=True, with_std=False)
+        try:
+            multiclass = pds.read_csv('./test_data/classification_multiclass.csv')
+            twoclass = pds.read_csv('./test_data/classification_twoclass.csv')
+        except OSError as exp:
+            os.system("python gen_synthetic_datasets.py")
+            multiclass = pds.read_csv('./test_data/classification_multiclass.csv')
+            twoclass = pds.read_csv('./test_data/classification_twoclass.csv')
+        finally:
+            # check this
+            self.da_mat = multiclass['Class_Vector'].values
+            self.da = twoclass['Class'].values
+            self.xmat_multi = multiclass.iloc[:, 5::].values
+            self.xmat = twoclass.iloc[:, 1::].values
+
+        # Set up the same scalers
+        y_scaler = ChemometricsScaler(0, with_std=False, with_mean=True)
         self.plsreg = ChemometricsPLS(ncomps=3, yscaler=y_scaler)
-        self.plslog = ChemometricsPLS_Logistic(ncomps=3)
-        self.plsqda = ChemometricsPLS_LDA(ncomps=3)
         self.plsda = ChemometricsPLSDA(ncomps=3)
+
         # Generate the dummy matrix so we can run the pls regression objects in the same conditions as
         # the discriminant ones
-
-        dummy_matrix = np.zeros((len(self.three_classdataset[0]), 3))
-        for col in range(3):
-            dummy_matrix[np.where(self.three_classdataset[1] == col), col] = 1
-        self.dummy_y = dummy_matrix
+        self.dummy_y = pds.get_dummies(self.da_mat).values
 
     def test_single_y(self):
 
-        self.plsreg.fit(self.twoclass_dataset[0], self.dummy_y)
-        self.plslog.fit(self.twoclass_dataset[0], self.twoclass_dataset[1])
-        self.plsqda.fit(self.twoclass_dataset[0], self.twoclass_dataset[1])
-        self.pldsda.fit(self.twoclass_dataset[0], self.twoclass_dataset[1])
+        self.plsreg.fit(self.xmat, self.da)
+        self.plsda.fit(self.xmat, self.da)
 
-        assert_array_equal(self.plsreg.scores_t, self.plslog.scores_t)
-        assert_array_equal(self.plsreg.scores_u, self.plslog.scores_u)
-        assert_array_equal(self.plsreg.rotations_cs, self.plslog.rotations_cs)
-        assert_array_equal(self.plsreg.rotations_ws, self.plslog.rotations_ws)
-        assert_array_equal(self.plsreg.weights_w, self.plslog.weights_w)
-        assert_array_equal(self.plsreg.weights_c, self.plslog.weights_c)
-        assert_array_equal(self.plsreg.loadings_p, self.plslog.loadings_p)
-        assert_array_equal(self.plsreg.loadings_q, self.plslog.loadings_q)
-        assert_array_equal(self.plsreg.beta_coeffs, self.plslog.beta_coeffs)
-        assert_array_equal(self.plsreg.modelParameters['R2Y'], self.plslog.modelParameters['PLS']['R2Y'])
-        assert_array_equal(self.plsreg.modelParameters['R2X'], self.plslog.modelParameters['PLS']['R2X'])
-        assert_array_equal(self.plsreg.modelParameters['SSX'], self.plslog.modelParameters['PLS']['SSX'])
-        assert_array_equal(self.plsreg.modelParameters['SSY'], self.plslog.modelParameters['PLS']['SSY'])
-        assert_array_equal(self.plsreg.modelParameters['SSXcomp'], self.plslog.modelParameters['PLS']['SSXcomp'])
-        assert_array_equal(self.plsreg.modelParameters['SSYcomp'], self.plslog.modelParameters['PLS']['SSYcomp'])
-        assert_array_equal(self.plsreg.modelParameters['R2Y'], self.plsqda.modelParameters['PLS']['R2Y'])
-        assert_array_equal(self.plsreg.modelParameters['R2X'], self.plsqda.modelParameters['PLS']['R2X'])
-        assert_array_equal(self.plsreg.modelParameters['SSX'], self.plsqda.modelParameters['PLS']['SSX'])
-        assert_array_equal(self.plsreg.modelParameters['SSY'], self.plsqda.modelParameters['PLS']['SSY'])
-        assert_array_equal(self.plsreg.modelParameters['SSXcomp'], self.plsqda.modelParameters['PLS']['SSXcomp'])
-        assert_array_equal(self.plsreg.modelParameters['SSYcomp'], self.plsqda.modelParameters['PLS']['SSYcomp'])
-        assert_array_equal(self.plsreg.modelParameters['R2Y'], self.plsda.modelParameters['PLS']['R2Y'])
-        assert_array_equal(self.plsreg.modelParameters['R2X'], self.plsda.modelParameters['PLS']['R2X'])
-        assert_array_equal(self.plsreg.modelParameters['SSX'], self.plsda.modelParameters['PLS']['SSX'])
-        assert_array_equal(self.plsreg.modelParameters['SSY'], self.plsda.modelParameters['PLS']['SSY'])
-        assert_array_equal(self.plsreg.modelParameters['SSXcomp'], self.plsda.modelParameters['PLS']['SSXcomp'])
-        assert_array_equal(self.plsreg.modelParameters['SSYcomp'], self.plsda.modelParameters['PLS']['SSYcomp'])
+        assert_allclose(self.plsreg.scores_t, self.plsda.scores_t)
+        assert_allclose(self.plsreg.scores_u, self.plsda.scores_u)
+        assert_allclose(self.plsreg.rotations_cs, self.plsda.rotations_cs)
+        assert_allclose(self.plsreg.rotations_ws, self.plsda.rotations_ws)
+        assert_allclose(self.plsreg.weights_w, self.plsda.weights_w)
+        assert_allclose(self.plsreg.weights_c, self.plsda.weights_c)
+        assert_allclose(self.plsreg.loadings_p, self.plsda.loadings_p)
+        assert_allclose(self.plsreg.loadings_q, self.plsda.loadings_q)
+        assert_allclose(self.plsreg.beta_coeffs, self.plsda.beta_coeffs)
+        assert_allclose(self.plsreg.modelParameters['R2Y'], self.plsda.modelParameters['PLS']['R2Y'])
+        assert_allclose(self.plsreg.modelParameters['R2X'], self.plsda.modelParameters['PLS']['R2X'])
+        assert_allclose(self.plsreg.modelParameters['SSX'], self.plsda.modelParameters['PLS']['SSX'])
+        assert_allclose(self.plsreg.modelParameters['SSY'], self.plsda.modelParameters['PLS']['SSY'])
+        assert_allclose(self.plsreg.modelParameters['SSXcomp'], self.plsda.modelParameters['PLS']['SSXcomp'])
+        assert_allclose(self.plsreg.modelParameters['SSYcomp'], self.plsda.modelParameters['PLS']['SSYcomp'])
 
     def test_multi_y(self):
-        self.plsreg.fit(self.three_classdataset[0], self.dummy_y)
-        self.plslog.fit(self.three_classdataset[0], self.three_classdataset[1])
 
-        assert_array_equal(self.plsreg.scores_t, self.plslog.scores_t)
-        assert_array_equal(self.plsreg.scores_u, self.plslog.scores_u)
-        assert_array_equal(self.plsreg.scores_u, self.plslog.scores_u)
-        assert_array_equal(self.plsreg.loadings_p, self.plslog.loadings_p)
-        assert_array_equal(self.plsreg.rotations_cs, self.plslog.rotations_cs)
-        assert_array_equal(self.plsreg.rotations_ws, self.plslog.rotations_ws)
-        assert_array_equal(self.plsreg.weights_w, self.plslog.weights_w)
-        assert_array_equal(self.plsreg.weights_c, self.plslog.weights_c)
-        assert_array_equal(self.plsreg.loadings_p, self.plslog.loadings_p)
-        assert_array_equal(self.plsreg.loadings_q, self.plslog.loadings_q)
-        assert_array_equal(self.plsreg.beta_coeffs, self.plslog.beta_coeffs)
-        assert_array_equal(self.plsreg.modelParameters['R2Y'], self.plslog.modelParameters['PLS']['R2Y'])
-        assert_array_equal(self.plsreg.modelParameters['R2X'], self.plslog.modelParameters['PLS']['R2X'])
-        assert_array_equal(self.plsreg.modelParameters['SSX'], self.plslog.modelParameters['PLS']['SSX'])
-        assert_array_equal(self.plsreg.modelParameters['SSY'], self.plslog.modelParameters['PLS']['SSY'])
-        assert_array_equal(self.plsreg.modelParameters['SSXcomp'], self.plslog.modelParameters['PLS']['SSXcomp'])
-        assert_array_equal(self.plsreg.modelParameters['SSYcomp'], self.plslog.modelParameters['PLS']['SSYcomp'])
-
+        self.plsreg.fit(self.xmat_multi, self.dummy_y)
+        self.plsda.fit(self.xmat_multi, self.da_mat)
+        
+        assert_allclose(self.plsreg.scores_t, self.plsda.scores_t)
+        assert_allclose(self.plsreg.scores_u, self.plsda.scores_u)
+        assert_allclose(self.plsreg.rotations_cs, self.plsda.rotations_cs)
+        assert_allclose(self.plsreg.rotations_ws, self.plsda.rotations_ws)
+        assert_allclose(self.plsreg.weights_w, self.plsda.weights_w)
+        assert_allclose(self.plsreg.weights_c, self.plsda.weights_c)
+        assert_allclose(self.plsreg.loadings_p, self.plsda.loadings_p)
+        assert_allclose(self.plsreg.loadings_q, self.plsda.loadings_q)
+        assert_allclose(self.plsreg.beta_coeffs, self.plsda.beta_coeffs)
+        assert_allclose(self.plsreg.modelParameters['R2Y'], self.plsda.modelParameters['PLS']['R2Y'])
+        assert_allclose(self.plsreg.modelParameters['R2X'], self.plsda.modelParameters['PLS']['R2X'])
+        assert_allclose(self.plsreg.modelParameters['SSX'], self.plsda.modelParameters['PLS']['SSX'])
+        assert_allclose(self.plsreg.modelParameters['SSY'], self.plsda.modelParameters['PLS']['SSY'])
+        assert_allclose(self.plsreg.modelParameters['SSXcomp'], self.plsda.modelParameters['PLS']['SSXcomp'])
+        assert_allclose(self.plsreg.modelParameters['SSYcomp'], self.plsda.modelParameters['PLS']['SSYcomp'])
+        
 
 if __name__ == '__main__':
     unittest.main()