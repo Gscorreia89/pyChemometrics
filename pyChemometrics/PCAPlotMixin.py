cedffb0bb74c6fe96112cf455e15f479e12587d7
Gscorreia89 <gscorreia89@gmail.com>
2018-02-01 19:43:49 +0000

    integrate better pltos from protocol to mixins, change cv defaults to no press_impute

--- pyChemometrics/PCAPlotMixin.py
+++ pyChemometrics/PCAPlotMixin.py
@@ -3,12 +3,13 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+from matplotlib.colors import Normalize
 import seaborn as sns
 import scipy.stats as st
 from sklearn.model_selection import KFold
 from copy import deepcopy
 from pyChemometrics.PlotMixin import PlotMixin
-
+import warnings
 
 class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
     """
@@ -17,7 +18,7 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
 
     """
 
-    def plot_scores(self, comps=[0, 1], color=None):
+    def plot_scores(self, comps=[0, 1], color=None, discrete=False):
         """
 
         Score plot figure wth an Hotelling T2.
@@ -28,25 +29,46 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
         """
         try:
             plt.figure()
-            if color is not None:
-                cmap = cm.get_cmap(name='Set1')
-                color = cmap(color)
-                comps = np.array(comps)
+
+            # Use a constant color if no color argument is passed
 
             t2 = self.hotelling_T2(alpha=0.05, comps=comps)
             outlier_idx = np.where(((self.scores[:, comps] ** 2) / t2 ** 2).sum(axis=1) > 1)[0]
 
             if len(comps) == 1:
-                plt.scatter(range(self.scores.shape[0]), self.scores[:, comps], color=color)
-                plt.scatter(range(self.scores.shape[0]), self.scores[outlier_idx, comps[0]], color=color, marker='x',
-                            s=1.5*mpl.rcParams['lines.markersize'] ** 2)
+                x_coord = np.arange(0, self.scores.shape[0])
+                y_coord = self.scores[:, comps[0]]
             else:
-                plt.scatter(self.scores[:, comps[0]], self.scores[:, comps[1]], color=color)
-                plt.scatter(self.scores[outlier_idx, comps[0]], self.scores[outlier_idx, comps[1]],
-                            color=color, marker='x', s=1.5*mpl.rcParams['lines.markersize'] ** 2)
+                x_coord = self.scores[:, comps[0]]
+                y_coord = self.scores[:, comps[1]]
 
-                t2 = self.hotelling_T2(comps=comps)
+            if color is None:
+                plt.scatter(x_coord, y_coord)
+                plt.scatter(x_coord[outlier_idx], y_coord[outlier_idx],
+                            marker='x', s=1.5 * mpl.rcParams['lines.markersize'] ** 2)
+            else:
+                if discrete is False:
+                    cmap = cm.jet
+                    cnorm = Normalize(vmin=min(color), vmax=max(color))
 
+                    plt.scatter(x_coord, y_coord, c=color, cmap=cmap, norm=cnorm)
+                    plt.scatter(x_coord[outlier_idx], y_coord[outlier_idx],
+                                c=color[outlier_idx], cmap=cmap, norm=cnorm, marker='x',
+                                s=1.5 * mpl.rcParams['lines.markersize'] ** 2)
+                    plt.colorbar()
+                else:
+                    cmap = cm.Set1
+                    subtypes = np.unique(color)
+                    for subtype in subtypes:
+                        subset_index = np.where(color == subtype)
+                        plt.scatter(x_coord[subset_index], y_coord[subset_index],
+                                    c=cmap(subtype), label=subtype)
+                    plt.legend()
+                    plt.scatter(x_coord[outlier_idx], y_coord[outlier_idx],
+                                c=color[outlier_idx], cmap=cmap, marker='x',
+                                s=1.5 * mpl.rcParams['lines.markersize'] ** 2)
+
+            if len(comps) == 2:
                 angle = np.arange(-np.pi, np.pi, 0.01)
                 x = t2[0] * np.cos(angle)
                 y = t2[1] * np.sin(angle)
@@ -54,14 +76,18 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
                 plt.axvline(c='k')
                 plt.plot(x, y, c='k')
 
-                xmin = np.minimum(min(self.scores[:, comps[0]]), np.min(x))
-                xmax = np.maximum(max(self.scores[:, comps[0]]), np.max(x))
-                ymin = np.minimum(min(self.scores[:, comps[1]]), np.min(y))
-                ymax = np.maximum(max(self.scores[:, comps[1]]), np.max(y))
+                xmin = np.minimum(min(x_coord), np.min(x))
+                xmax = np.maximum(max(x_coord), np.max(x))
+                ymin = np.minimum(min(y_coord), np.min(y))
+                ymax = np.maximum(max(y_coord), np.max(y))
 
                 axes = plt.gca()
                 axes.set_xlim([(xmin + (0.2 * xmin)), xmax + (0.2 * xmax)])
                 axes.set_ylim([(ymin + (0.2 * ymin)), ymax + (0.2 * ymax)])
+            else:
+                plt.axhline(y=t2, c='k', ls='--')
+                plt.axhline(y=-t2, c='k', ls='--')
+                plt.legend(['Hotelling $T^{2}$ 95% limit'])
 
         except (ValueError, IndexError) as verr:
             print("The number of components to plot must not exceed 2 and the component choice cannot "
@@ -70,10 +96,10 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
 
         plt.title("PCA score plot")
         if len(comps) == 1:
-            plt.xlabel("PC[{0}] - Variance Explained : {1:.2} %".format((comps[0] + 1), self.modelParameters['VarExpRatio']*100))
+            plt.xlabel("PC[{0}] - Variance Explained : {1:.2f} %".format((comps[0] + 1), self.modelParameters['VarExpRatio']*100))
         else:
-            plt.xlabel("PC[{0}] - Variance Explained : {1:.2} %".format((comps[0] + 1), self.modelParameters['VarExpRatio'][comps[0]]*100))
-            plt.ylabel("PC[{0}] - Variance Explained : {1:.2} %".format((comps[1] + 1), self.modelParameters['VarExpRatio'][comps[1]]*100))
+            plt.xlabel("PC[{0}] - Variance Explained : {1:.2f} %".format((comps[0] + 1), self.modelParameters['VarExpRatio'][comps[0]]*100))
+            plt.ylabel("PC[{0}] - Variance Explained : {1:.2f} %".format((comps[1] + 1), self.modelParameters['VarExpRatio'][comps[1]]*100))
         plt.show()
         return None
 
@@ -116,14 +142,16 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
         plt.figure()
         models = list()
 
-        for ncomps in range(1, total_comps + 1):
-            currmodel = deepcopy(self)
-            currmodel.ncomps = ncomps
-            currmodel.fit(x)
-            currmodel.cross_validation(x, outputdist=False, cv_method=cv_method)
-            models.append(currmodel)
+        with warnings.catch_warnings():
+            warnings.simplefilter(action='ignore', category=DataConversionWarning)
+            for ncomps in range(1, total_comps + 1):
+                currmodel = deepcopy(self)
+                currmodel.ncomps = ncomps
+                currmodel.fit(x)
+                currmodel.cross_validation(x, outputdist=False, cv_method=cv_method)
+                models.append(currmodel)
 
-        q2 = np.array([x.cvParameters['Q2X'] for x in models])
+        q2 = np.array([x.cvParameters['Q2'] for x in models])
         r2 = np.array([x.modelParameters['R2X'] for x in models])
 
         plt.bar([x - 0.1 for x in range(1, total_comps + 1)], height=r2, width=0.2)
@@ -163,13 +191,16 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
 
         q2x = np.zeros((total_comps, repeats))
 
-        for ncomps in range(1, total_comps + 1):
-            for rep in range(repeats):
-                currmodel = deepcopy(self)
-                currmodel.ncomps = ncomps
-                currmodel.fit(x)
-                currmodel.cross_validation(x, cv_method=cv_method, outputdist=False)
-                q2x[ncomps - 1, rep] = currmodel.cvParameters['Q2X']
+        with warnings.catch_warnings():
+            warnings.simplefilter(action='ignore', category=DataConversionWarning)
+
+            for ncomps in range(1, total_comps + 1):
+                for rep in range(repeats):
+                    currmodel = deepcopy(self)
+                    currmodel.ncomps = ncomps
+                    currmodel.fit(x)
+                    currmodel.cross_validation(x, cv_method=cv_method, outputdist=False)
+                    q2x[ncomps - 1, rep] = currmodel.cvParameters['Q2']
 
         plt.figure()
         ax = sns.violinplot(data=q2x.T, palette="Set1")
@@ -180,6 +211,43 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
         plt.show()
 
         return q2x
+
+    def plot_loadings(self, component=1, bar=False, sigma=2):
+        """
+        Loading plot figure for the selected component. With uncertainty estimation if the cross validation method
+        has been called before.
+
+        :param float component: Component to plot loadings
+        :param boolean bar: Whether to use line or bar plot
+        :param float sigma: Multiple of standard deviation to plot
+        :return: Loading plot figure
+        """
+        # Adjust the indexing so user can refer to component 1 as component 1 instead of 0
+        component -= 1
+        plt.figure()
+
+        # For "spectrum/continuous like plotting"
+        if bar is False:
+            ax = plt.plot(self.loadings[component, :])
+            if self.cvParameters is not None:
+                plt.fill_between(range(self.loadings[component, :].size),
+                                 self.cvParameters['Mean_Loadings'][component] - sigma*self.cvParameters['Stdev_Loadings'][component],
+                                 self.cvParameters['Mean_Loadings'][component] + sigma*self.cvParameters['Stdev_Loadings'][component],
+                                 alpha=0.2, color='red')
+        # To use with barplots for other types of data
+        else:
+            if self.cvParameters is not None:
+                plt.errorbar(range(self.loadings_p[:, component].size),
+                             height=self.cvParameters['Mean_Loadings'][:, component],
+                             yerr=2 * self.cvParameters['Stdev_Loadings'][:, component],
+                             width=0.2)
+            else:
+                plt.bar(range(self.loadings[component, :].size), height=self.loadings[component, :], width=0.2)
+        plt.xlabel("Variable No")
+        plt.ylabel("Loading for PC{0}".format((component + 1)))
+        plt.show()
+
+        return None
 
     def plot_dmodx(self, x, alpha=0.05):
         """
@@ -193,8 +261,10 @@ class PCAPlotMixin(PlotMixin, metaclass=ABCMeta):
 
         try:
             dmodx = self.dmodx(x)
-
-            dcrit = self._dmodx_fcrit(x, alpha)
+            # Degrees of freedom for the PCA model (denominator in F-stat) calculated as suggested in
+            # Faber, Nicolaas (Klaas) M., Degrees of freedom for the residuals of a
+            # principal component analysis - A clarification, Chemometrics and Intelligent Laboratory Systems 2008
+            dcrit = st.f.ppf(1-alpha, x.shape[1] - self.ncomps - 1, (x.shape[0] - self.ncomps - 1)*(x.shape[1] - self.ncomps))
             outlier_idx = self.outlier(x, measure='DmodX')
             plt.figure()
             x_axis = np.array([x for x in range(x.shape[0])])
