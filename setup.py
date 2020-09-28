29eb9dab06fafa03c63e4acc9284125c08d93cb1
gscorreia89 <gscorreia89@gmail.com>
2020-09-28 22:48:18 +0100

    rmeove deprecated skip_cleanup

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     documentation='http://pychemometrics.readthedocs.io/en/stable/',
     license='BSD 3-Clause License',
     author='Gonçalo Correia',
+    setup_requires=['wheel'],
     author_email='gscorreia89@gmail.com',
     description='The pyChemometrics provides objects which wrap pre-existing '
                 'scikit-learn PCA and PLS algorithms and adds model assessment metrics and functions '
