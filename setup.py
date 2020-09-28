3c89350f4ac5cb2467d6d662963988beb1640ae7
gscorreia89 <gscorreia89@gmail.com>
2020-09-28 22:42:19 +0100

    rmeove deprecated skip_cleanup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyChemometrics',
-    version='0.13.7',
+    version='0.13.3',
     packages=['pyChemometrics'],
     url='https://github.com/Gscorreia89/pyChemometrics/',
     documentation='http://pychemometrics.readthedocs.io/en/stable/',
